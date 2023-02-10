ce7c18f4e0e492155dc0183beac1cc9759cb4fa5
preeti <pritich0896@gmail.com>
2023-02-11 01:04:48 +0530

    [Preeti] : Checking fibonacci a series

---
+++ Logical Programs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programs");
+            Console.WriteLine("Choose a Option \n" + "1: FlipCoin Problem");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.Fibonacci();
+                    break;
+                default:
+                    Console.WriteLine("Please Choose a Correct option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
