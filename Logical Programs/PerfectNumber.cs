f9bcbca545e4aabcaddf8318b71e6b6061cd1ad8
preeti <pritich0896@gmail.com>
2023-02-11 01:52:36 +0530

    [Preeti] : Checking a number Perfect or not

---
+++ Logical Programs/PerfectNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    public class PerfectNumber
+    {
+        public void Perfect()
+        {
+            Console.WriteLine("Enter a number to Check Perfect or Not");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i < Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("\n{0}", i);
+                }
+            }
+            Console.WriteLine("The Sum of Divisors is : " + sum);
+            if (sum == Num)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;  //below statment shows in Green color
+                Console.WriteLine("Given Number is Perfect Number");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;    //below statment shows in Red color
+                Console.WriteLine("The Given Number iS Not a Perfect NUmber");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Logical Programs/Program.cs
+++ Logical Programs/Program.cs
@@ -11,7 +11,7 @@ namespace Logical_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Logical Programs");
-            Console.WriteLine("Choose a Option \n" + "1: FlipCoin Problem");
+            Console.WriteLine("Choose a Option \n" + "1: FlipCoin Problem \n2: Perfect Number");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -19,6 +19,10 @@ namespace Logical_Programs
                 case 1:
                     FibonacciSeries fibonacciSeries = new FibonacciSeries();
                     fibonacciSeries.Fibonacci();
+                    break;
+                case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    perfectNumber.Perfect();
                     break;
                 default:
                     Console.WriteLine("Please Choose a Correct option");
